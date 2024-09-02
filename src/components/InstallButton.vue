bc310a24592313a4b5256c8943aac290643d627a
Julian Fickers <96172826+julianfickerseq@users.noreply.github.com>
2024-09-02 09:45:39 +0200

    addded safari install

--- src/components/InstallButton.vue
+++ src/components/InstallButton.vue
@@ -48,4 +48,11 @@ const firefox = navigator.userAgent.indexOf('Firefox') !== -1
       <v-icon icon="mdi-plus-box-outline"></v-icon>
     </div>
   </v-footer>
+  <v-footer v-if="firefox" class="d-flex flex-column justify-center" color="cyan-darken-3"
+    ><div class="w-100">To install the app click on : <v-icon icon="mdi-menu"></v-icon></div>
+    <div class="w-100">
+      Then on : "Add to home screen"
+      <v-icon icon="mdi-plus-box-outline"></v-icon>
+    </div>
+  </v-footer>
 </template>
