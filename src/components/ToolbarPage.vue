ed0c8dcfb0e5b786f7c243deeca1e9da72ce8941
Julian Fickers <96172826+julianfickerseq@users.noreply.github.com>
2024-09-03 20:45:36 +0200

    version number added

--- src/components/ToolbarPage.vue
+++ src/components/ToolbarPage.vue
@@ -5,6 +5,9 @@ const router = useRouter()
 const refresh = () => {
   location.reload(true)
 }
+const about = () => {
+  alert('version: 1.0.1')
+}
 </script>
 <template>
   <v-toolbar class="top-0 position-absolute left-0">
@@ -13,6 +16,7 @@ const refresh = () => {
     <v-btn id="menu-activator" icon="mdi-dots-vertical"></v-btn>
     <v-menu activator="#menu-activator">
       <v-list>
+        <v-list-item title="About" @click="about"> </v-list-item>
         <v-list-item title="Refresh App" @click="refresh"> </v-list-item>
       </v-list>
     </v-menu>
